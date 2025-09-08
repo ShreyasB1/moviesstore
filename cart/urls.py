148715860a662ac44d36139de24dc9c6a31f9f72
Shreyas Balawatri <shreyasb@ipsec-10-2-1-29.vpn.gatech.edu>
2025-09-07 23:20:55 -0400

    first version

---
+++ cart/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('<int:id>/add/', views.add, name='cart.add'),
+    path('', views.index, name='cart.index'),
+    path('clear/', views.clear, name='cart.clear'),
+    path('purchase/', views.purchase,
+        name='cart.purchase'),
+]
+def clear(request):
+    request.session['cart'] = {}
+    return redirect('cart.index')
