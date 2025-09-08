bdbbeb6ce951ae278e4d1457c7d3668496e1f243
Shreyas Balawatri <shreyasb@Shreyass-MacBook-Pro.local>
2025-09-08 15:59:47 -0400

    Initial commit of all code

--- cart/urls.py
+++ cart/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('<int:id>/add/', views.add, name='cart.add'),
     path('', views.index, name='cart.index'),
+    path('<int:id>/add/', views.add, name='cart.add'),
     path('clear/', views.clear, name='cart.clear'),
-    path('purchase/', views.purchase,
-        name='cart.purchase'),
+    path('purchase/', views.purchase, name='cart.purchase'),
 ]
-def clear(request):
-    request.session['cart'] = {}
-    return redirect('cart.index')
